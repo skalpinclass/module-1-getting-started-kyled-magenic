d646a6021003ad3369b203cfdc0638b8fe488231
Kyle Deak <kyled@magenic.com>
2019-07-11 11:45:48 -0500

    module 1 initial commit

---
+++ ConsoleBlog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+                var blog = new Blog { Url = "http://sample.com" };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+
+            using(var db = new BloggingContext())
+            {
+                var blogs = db.Blogs;
+                foreach (var blog in blogs)
+                    Console.WriteLine($"Blog item found {blog.BlogId}.");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
